44a9c527bd92d55399a2f2b8ea11ad47566d864f
wuyunxiao <1442017187@qq.com>
2022-03-03 01:46:57 +0800

    as

--- src/main/java/com/itheima/aop/sfsd.java
+++ src/main/java/com/itheima/aop/sfsd.java
@@ -4,13 +4,13 @@ package com.itheima.aop;
  * @author 侯文远
  * @version V1.0
  * @Package com.itheima.aop
- * @date 2022/3/3 1:35
+ * @date 2022/3/3 1:45
  * @Copyright © 2016-2017 奥琦玮信息科技（北京）有限公司
  */
-public class aaa {
+public class sfsd {
     public static void main(String[] args) {
-        System.out.println("jsdkf");
-        System.out.println("jss21sdkf");
-        System.out.println("jsd22kf");
+        System.out.println("jfsd");
+        System.out.println("jfs1d");
+
     }
 }
